2891d133d76b2cd337bd5ab5f80f5cd886a39f28
abishek.g <abishek.g@mail.aquilogix.com>
2019-02-20 15:42:28 +0530

    Initial Commit

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+export class App extends Component {
+
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+
+}
+
+const mapDispatchToProps = {
+
+}
+
+export default (App)
