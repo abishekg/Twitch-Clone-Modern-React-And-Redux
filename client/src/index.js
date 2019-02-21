ba0041cfedc52c6d40f748bb02e9825b4bb3641b
abishek.g <abishek.g@mail.aquilogix.com>
2019-02-21 15:46:08 +0530

    Redux-Form Implemented

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
-
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 
 ReactDOM.render(<div>
